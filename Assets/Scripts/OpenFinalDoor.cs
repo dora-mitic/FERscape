7c06ea9549ed152747de73a4409b7ffe98755bbc
korinatopic <korina.topic@fer.unizg.hr>
2026-01-20 20:46:16 +0100

    popravila ovaj end screen
    sve ide kak treba samo se jos ne moze doc do ureda

---
+++ Assets/Scripts/OpenFinalDoor.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenFinalDoor : MonoBehaviour
+{
+    public void openPobjeda()
+    {
+        SceneManager.LoadScene("End screen");   
+    }
+
+}
