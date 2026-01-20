10a2b277d07b9a67c274665c38956ec9231f9c8a
PotjehJurekovic <pj55314@fer.hr>
2026-01-20 14:32:22 +0100

    Dizalo

---
+++ Assets/Scripts/Dizalo/DKAController.cs
@@ -0,0 +1,98 @@
+using UnityEngine;
+
+public class DKAController : MonoBehaviour
+{
+    // pocetno stanje (ne error)
+    public int startState = 1;
+
+    // trenutno stanje
+    public int currentState;
+
+    // tocno stanje
+    public int winState = 7;
+
+    
+    
+    public GameObject pocetakLifta;
+    public GameObject krajLifta;
+
+    // prijelazi: [stanje, input]
+    // input: 1, 2, 3 -> indeksi 0,1,2
+    // stanje 0 = error - vracanje na prizemlje
+    private int[,] prijelazi =
+    {
+        // 1  2  3
+        { 0, 0, 0 }, // stanje 0 (error)
+        { 3, 2, 0 }, // stanje 1
+        { 5, 0, 1 }, // stanje 2
+        { 3, 4, 0 }, // stanje 3 (krivi kat)
+        { 6, 3, 5 }, // stanje 4
+        { 0, 5, 6 }, // stanje 5 (krivi kat)
+        { 0, 7, 7 }, // stanje 6
+        { 7, 7, 7 }  // stanje 7 (tocan kat)
+    };
+
+    void Start()
+    {
+        ResetDKA();
+    }
+
+    // gumb 1
+    public void Input1()
+    {
+        prijelaz(1);
+    }
+
+    // gumb 2
+    public void Input2()
+    {
+        prijelaz(2);
+    }
+
+    // gumb 3
+    public void Input3()
+    {
+        prijelaz(3);
+    }
+
+    // gumb 0 ili error
+    public void ResetDKA()
+    {
+        currentState = startState;
+        Debug.Log("reset -> stanje " + currentState);
+    }
+
+    private void prijelaz(int input)
+    {
+        int inputIndex = input - 1;
+        int nextState = prijelazi[currentState, inputIndex];
+
+        if (nextState == 0)
+        {
+            Debug.Log("error prijelaz -> reset");
+            ResetDKA();
+            Debug.Log("Kraj puzzlea, kreni ispocetka");
+            return;
+        }
+
+        currentState = nextState;
+        Debug.Log("input " + input + " -> stanje " + currentState);
+
+        provjeriStanje();
+    }
+
+    private void provjeriStanje()
+    {
+        if(currentState == 3 | currentState == 5){
+            Debug.Log("Tocno stanje, krivi kat - ovdje ide text bubble kada implementiramo");
+            return;
+        }
+        if (currentState == winState )
+        {
+            Debug.Log("pobjeda - tocno stanje 7");
+            
+            krajLifta.SetActive(true);
+            pocetakLifta.SetActive(false);
+        }
+    }
+}
