7ad8356e6a49881eb355c80387e529ca02d4b6fb
korinatopic <korina.topic@fer.unizg.hr>
2026-01-21 13:24:52 +0100

    ---

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -9,7 +9,6 @@ public class InventoryManager : MonoBehaviour
 
     public void AddItem(Item item)
     {
-        // Find any empty slot
         for (int i = 0; i < inventorySlots.Length; i++) {
             InventorySlot slot = inventorySlots[i];
             InventoryItem itemInSlot = slot.GetComponentInChildren<InventoryItem>();
